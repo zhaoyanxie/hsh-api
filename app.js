a260b711bd80dd6cb8857e4cfc1a003b213ef5d0
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-09-14 12:45:41 +0800

    Added cors options

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+
 const index = require("./routes/index");
 const ourProducts = require("./routes/ourproducts");
 
 const app = express();
 app.use(express.json());
+
+var corsOptions = {
+  origin: [/http:\/\/localhost:.*/, /http[s]*:\/\/zhaoyanxie.*\.github.io/],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 index(app);
 ourProducts(app);
